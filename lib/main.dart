a94462a1ed9f3c50eb2839698b53f30d588a6405
kushal <kushalthapa1373@gmail.com>
2024-12-11 10:30:40 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+
+import 'package:calculator_app/view/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
---
+++ lib/view/app.dart
@@ -0,0 +1,17 @@
+
+import 'package:calculator_app/view/calculator_app.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CalculatorView(),
+    );
+  }
+}
